4c4f48ba9795bac7c4f822dac35c3c75039abc19
ACERTO\mauricio.souza <mauricio.souza@meuacerto.com.br>
2023-04-19 12:49:56 -0300

    Add Options for ResponseCompression

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System.Threading.RateLimiting;
 using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,11 +8,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddResponseCompression();
+
+builder.Services.AddResponseCompression(options =>
+{
+    // Configure os algoritmos de compressão desejados
+    options.Providers.Add<GzipCompressionProvider>();
+
+    // Opções adicionais de compressão
+    options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/json" });
+    options.EnableForHttps = true;
+});
+
 builder.Services.AddRateLimiter(options =>
 {
+    // Adiciona status code 429 quando limite atingido
     options.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
 
+    // Adiciona política de janela fixa
     options.AddFixedWindowLimiter(policyName: "FixedWindowRateLimiter", options =>
     {
         options.PermitLimit = 5;
@@ -20,6 +33,7 @@ builder.Services.AddRateLimiter(options =>
         options.QueueLimit = 0;
     });
 
+    // Adiciona mensagem de limite atingido ao usuario
     options.OnRejected = async (context, token) =>
     {
         context.HttpContext.Response.StatusCode = 429;
